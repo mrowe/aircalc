0e1d74198407239824f724bcd55741eb410c7cd6
Michael Rowe <mrowe@mojain.com>
2018-12-09 11:19:41 +1100

    Model real numbers

---
+++ src/main/java/com/mojain/aircalc/Real.java
@@ -0,0 +1,56 @@
+package com.mojain.aircalc;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+/**
+ * A real number.
+ *
+ * From the requirements:
+ *
+ *    "Numbers should be stored on the stack to at least 15 decimal places of precision, but displayed to 10 decimal
+ *     places (or less if it causes no loss of precision)."
+ *
+ *  I'm choosing to interpret this as 10/15 places of precision over all, not _after_ the decimal point.
+ */
+public class Real {
+
+    private static final int SCALE = 15;
+
+    private final BigDecimal value;
+
+    public Real(double value) {
+        this.value = new BigDecimal(value, new MathContext(SCALE));
+    }
+
+    private Real(BigDecimal value) {
+        this.value = value.setScale(SCALE, RoundingMode.HALF_UP);
+    }
+
+    public double asDouble() {
+        return value.doubleValue();
+    }
+
+    public String toString() {
+        return value.setScale(10, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString();
+    }
+
+    // delegate mathematical operations to underlying BigDecimal
+
+    public Real add(Real augend) {
+        return new Real(value.add(augend.value));
+    }
+
+    public Real subtract(Real subtrahend) {
+        return new Real(value.subtract(subtrahend.value));
+    }
+
+    public Real multiply(Real multiplicand) {
+        return new Real(value.multiply(multiplicand.value));
+    }
+
+    public Real divide(Real divisor) {
+        return new Real(value.divide(divisor.value, SCALE, RoundingMode.HALF_UP));
+    }
+}
---
+++ src/test/java/com/mojain/aircalc/RealTest.java
@@ -0,0 +1,67 @@
+package com.mojain.aircalc;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class RealTest {
+
+    @Test
+    public void shouldStoreAValue(){
+        assertEquals(123.0, new Real(123.0).asDouble(), 0.0);
+    }
+
+    @Test
+    public void shouldStoreAtLeast15DigitsOfPrecision() {
+        assertEquals(0.0123456789123456, new Real(0.0123456789123456).asDouble(), 0);
+    }
+
+    @Test
+    public void shouldMaintainPrecisionAcrossOperations() {
+        Real real = new Real(2.0);
+        assertRealEquals(2.0, real.divide(new Real(2.0)).multiply(new Real(2.0)));
+    }
+
+    @Test
+    public void shouldHandleRepeatingDecimals() {
+        Real real = new Real(1.0);
+        assertRealEquals(0.333333333333333, real.divide(new Real(3.0)));
+    }
+
+    @Test
+    public void shouldBeAbleToAdd() {
+        assertRealEquals(3.0, new Real(1).add(new Real(2)));
+    }
+
+    @Test
+    public void shouldBeAbleToSubtract() {
+        assertRealEquals(1.0, new Real(3).subtract(new Real(2)));
+    }
+
+    @Test
+    public void shouldFormatIntegerWithoutDecimals() {
+        Real real = new Real(123.0);
+        assertEquals("123", real.toString());
+    }
+
+    @Test
+    public void shouldFormatDecimalWithSufficientPrecision() {
+        assertEquals("1.23", new Real(1.23).toString());
+    }
+
+    @Test
+    public void shouldFormatDecimalWithSufficientPrecision1() {
+        assertEquals("12.3", new Real(12.3).toString());
+    }
+
+    @Test
+    public void shouldFormatDecimalWithoutExcessivePrecision() {
+        assertEquals("0.0123456789", new Real(0.012345678912345).toString());
+    }
+
+    private void assertRealEquals(double expected, Real actual) {
+        assertEquals(expected, actual.asDouble(), 0.000000000000001);
+    }
+}
