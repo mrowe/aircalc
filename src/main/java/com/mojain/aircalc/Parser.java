b3f2dae35b69e65a8499b7102a027207a2ebe32c
Michael Rowe <mrowe@mojain.com>
2018-12-11 07:44:51 +1100

    Parse an input string

---
+++ src/main/java/com/mojain/aircalc/Parser.java
@@ -0,0 +1,61 @@
+package com.mojain.aircalc;
+
+import com.mojain.aircalc.commands.*;
+import com.mojain.aircalc.operators.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Parser {
+
+    List<Command> parse(String input) {
+        List<Command> commands = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(input);
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+
+            if (token.equalsIgnoreCase("quit")) {
+                commands.add(new Quit());
+                continue;
+            }
+
+            if (token.equalsIgnoreCase("clear")) {
+                commands.add(new Clear());
+                continue;
+            }
+
+            Real number = tryToParseNumber(token);
+            if (number != null) {
+                commands.add(new Push(number));
+                continue;
+            }
+
+            Operator operator = tryToParseOperator(token);
+            if (operator != null) {
+                commands.add(new Eval(operator));
+                continue;
+            }
+
+            System.err.format("Could not parse token: %s\n", token);
+        }
+        return commands;
+    }
+
+    private Real tryToParseNumber(String token) {
+        try {
+            return new Real(token);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    private Operator tryToParseOperator(String token) {
+        if (token.equalsIgnoreCase("sqrt")) return new SquareRoot();
+        if (token.equalsIgnoreCase("+")) return new Plus();
+        if (token.equalsIgnoreCase("-")) return new Minus();
+        if (token.equalsIgnoreCase("*")) return new Times();
+        if (token.equalsIgnoreCase("/")) return new Divides();
+        return null;
+    }
+}
---
+++ src/test/java/com/mojain/aircalc/ParserTest.java
@@ -0,0 +1,78 @@
+package com.mojain.aircalc;
+
+import com.mojain.aircalc.commands.*;
+import com.mojain.aircalc.operators.Plus;
+import com.mojain.aircalc.operators.SquareRoot;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ParserTest {
+
+    private Parser parser;
+
+    @Before
+    public void setUp() {
+        parser = new Parser();
+    }
+
+    @Test
+    public void shouldHandleEmptyInput() {
+        assertTrue(parser.parse("").isEmpty());
+    }
+
+    @Test
+    public void shouldParseQuitCommand() {
+        assertEquals(Collections.singletonList(new Quit()), parser.parse("quit"));
+    }
+
+    @Test
+    public void shouldParseClearCommand() {
+        assertEquals(Collections.singletonList(new Clear()), parser.parse("clear"));
+    }
+
+    @Test
+    public void shouldParseSingleNumber() {
+        assertEquals(Collections.singletonList(new Push(new Real(1))), parser.parse("1"));
+    }
+
+    @Test
+    public void shouldParseSingleDecimal() {
+        assertEquals(Collections.singletonList(new Push(new Real(0.12345))), parser.parse("0.12345"));
+    }
+
+    @Test
+    public void shouldParseSingleOperator() {
+        assertEquals(Collections.singletonList(new Eval(new Plus())), parser.parse("+"));
+    }
+
+    @Test
+    public void shouldParseSingleSqrtOperator() {
+        assertEquals(Collections.singletonList(new Eval(new SquareRoot())), parser.parse("sqrt"));
+    }
+
+    @Test
+    public void shouldParseASequenceOfOperandsAndOperators() {
+        List result = parser.parse("1 2 + clear");
+        assertEquals(4, result.size());
+        assertEquals(new Push(new Real(1)), result.get(0));
+        assertEquals(new Push(new Real(2)), result.get(1));
+        assertEquals(new Eval(new Plus()), result.get(2));
+        assertEquals(new Clear(), result.get(3));
+    }
+
+    @Test
+    public void shouldIgnoreBadNumbersAndOperators() {
+        List result = parser.parse("clear 1 z2 3 % quit");
+        assertEquals(4, result.size());
+        assertEquals(new Clear(), result.get(0));
+        assertEquals(new Push(new Real(1)), result.get(1));
+        assertEquals(new Push(new Real(3)), result.get(2));
+        assertEquals(new Quit(), result.get(3));
+    }
+}
